cb371998fed9bd57d349523a2b43cbefb95ca4a5
Nicole Haraj <nicoleharaj@outlook.com>
2023-10-04 22:49:05 -0400

    define tailwind styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,47 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      tablet: '768px',
+      desktop: '1440px'
+    },
+
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-instrument-sans)", "sans-serif"],
+      },
+    },
+    fontSize: {
+      "body-s": ["12px", "150%"],
+      base: ["16px", "150%"],
+      "heading-sm": ["16px", "150%"],
+      "heading-md-web": ["32px", "150%"],
+      "heading-md-mobile": ["24px", "150%"],
+    },
+
+    boxShadow: {
+      none: "0 0 #0000",
+      active: "0 0 32px 0 rgba(99, 60, 255, 0.25)",
+      DEFAULT: "0 0 32px 0 rgba(0, 0, 0, 0.1)",
+    },
+
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      gray: {
+        dark: "#333333",
+        DEFAULT: "#737373",
+        border: "#D9D9D9",
+        light: "#FAFAFA",
+      },
+      white: "#ffffff",
+      red: "#FF3939",
+      purple: {
+        DEFAULT: "#633CFF",
+        hover: "#BEADFF",
+        light: "#EFEBFF",
+      },
+    },
   },
   plugins: [],
 };
