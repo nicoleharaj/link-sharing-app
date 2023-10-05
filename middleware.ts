a8f35dc026a78320f99ef14bcc8da40de80dbfb9
Nicole Haraj <nicoleharaj@outlook.com>
2023-10-05 18:50:15 -0400

    implement auth

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,8 @@ import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
 import { NextResponse } from "next/server";
 
 import type { NextRequest } from "next/server";
-import type { Database } from "@/types/supabase";
+import type { Database } from "@/lib/types/supabase";
+
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
