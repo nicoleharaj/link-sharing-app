c9560c20f039a411e70823866570b0ade296794f
Nicole Haraj <nicoleharaj@outlook.com>
2023-11-07 12:35:11 -0500

    add phone component

--- src/app/components/Mockup/index.tsx
+++ src/app/components/Mockup/index.tsx
@@ -1,16 +1,16 @@
-import Image from "next/image";
+"use client";
 import Container from "../UI/Container";
+import UserInfo from "../UserInfo";
+import Phone from "./Phone";
 
 export default function Mockup() {
+  
+
+
   return (
-    <Container className="invisible hidden h-full  desktop:visible desktop:flex desktop:w-1/2 desktop:flex-col desktop:items-center desktop:justify-center">
-      <Image
-        src="/images/illustration-phone-mockup.svg"
-        alt="mockup"
-        width={307}
-        height={631}
-        className="select-none"
-      />
+    <Container className="invisible hidden h-full  desktop:visible desktop:flex desktop:w-1/2 desktop:flex-col desktop:items-center">
+      <div className="z-20 mt-14">{/* <UserInfo /> */}</div>
+      <Phone/>
     </Container>
   );
 }
