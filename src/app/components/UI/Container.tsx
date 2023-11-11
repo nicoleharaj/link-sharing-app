4123e6d2f1bd3caf3a7947eb7b00c929184945cc
Nicole Haraj <nicoleharaj@outlook.com>
2023-11-11 13:23:14 -0500

    add forwardRef to component

--- src/app/components/UI/Container.tsx
+++ src/app/components/UI/Container.tsx
@@ -1,18 +1,21 @@
-import { ReactNode } from "react";
+import { forwardRef } from "react";
 import { twMerge } from "tailwind-merge";
 
-export default function Container({
-  children,
-  className,
-}: {
-  children?: ReactNode;
-  className?: string;
-}) {
+type ContainerProps = React.HTMLProps<HTMLDivElement>;
+
+const Container = forwardRef<HTMLDivElement, ContainerProps>(function Container(
+  { children, className, ...props },
+  ref,
+) {
   return (
     <div
+      ref={ref}
       className={twMerge(["rounded-xl bg-white p-6 tablet:p-10", className])}
+      {...props}
     >
       {children}
     </div>
   );
-}
+});
+
+export default Container;
