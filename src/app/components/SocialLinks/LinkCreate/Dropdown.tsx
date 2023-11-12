698e0b846013734edcde7fa17411c6d9bf2c00f0
Nicole Haraj <nicoleharaj@outlook.com>
2023-11-11 19:27:34 -0500

    fix onBlur behavior

--- src/app/components/SocialLinks/LinkCreate/Dropdown.tsx
+++ src/app/components/SocialLinks/LinkCreate/Dropdown.tsx
@@ -1,31 +1,64 @@
-import { Dispatch, SetStateAction, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
 import Image from "next/image";
-import { platform, platforms } from "./platforms";
-import { validSocials } from "..";
+import { iconMap, platform, platforms } from "./platforms";
+import { SocialLink, validSocials } from "..";
 
 export default function Dropdown({
-  setCurrentPlatform,
+  linkId,
   currentPlatform,
+  setCurrentPlatform,
+  setLinks,
 }: {
-  setCurrentPlatform: Dispatch<SetStateAction<platform>>;
+  linkId: number;
   currentPlatform: platform;
+  setCurrentPlatform: Dispatch<SetStateAction<platform>>;
+  setLinks: Dispatch<SetStateAction<SocialLink[]>>;
 }) {
   const [showMenu, setShowMenu] = useState(false);
+  const menuRef = useRef<HTMLUListElement>(null);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  const handleClickOutside = (e: MouseEvent) => {
+    if (
+      menuRef.current &&
+      !menuRef.current.contains(e.target as Node) &&
+      buttonRef.current &&
+      !buttonRef.current.contains(e.target as Node)
+    ) {
+      setShowMenu(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   const handleSelect = (platform: {
     value: validSocials;
     label: string;
-    icon: string;
     placeholder: string;
     regex: RegExp;
   }) => {
     setCurrentPlatform(platform);
     setShowMenu(false);
+  
+    setLinks((links) => {
+      return links.map((item) =>
+        linkId === item.id ? { ...item, type: platform.value } : item
+      );
+    });
   };
+
+  const CurrentPlatformIcon = iconMap[currentPlatform.value];
 
   return (
     <>
       <button
+        ref={buttonRef}
         type="button"
         onClick={() => setShowMenu(!showMenu)}
         className={
@@ -33,7 +66,7 @@ export default function Dropdown({
         }
         aria-label="Platform select"
       >
-        <Image src={currentPlatform.icon} alt="link" width={16} height={16} />
+        <CurrentPlatformIcon className="text-gray" />
         <span className="flex-grow text-left">{currentPlatform.label}</span>
         <Image
           src="/images/icon-chevron-down.svg"
@@ -44,28 +77,31 @@ export default function Dropdown({
         />
       </button>
       {showMenu && (
-        <ul className="absolute z-20 mt-2 h-[148px] w-[calc(100%-3rem)] overflow-y-scroll rounded-lg border border-gray-border bg-white px-4 py-3 shadow tablet:w-[calc(100%-5rem)]">
-          {platforms.map((platform) => (
-            <li
-              key={platform.value}
-              className="border-b border-gray-border py-3 first-of-type:py-0 first-of-type:pb-3 last-of-type:border-b-0 last-of-type:py-0 last-of-type:pt-3 only-of-type:py-0 only-of-type:pb-0 only-of-type:pt-0"
-            >
-              <button
-                value={platform.value}
-                className="flex w-full items-center gap-3"
-                onClick={() => handleSelect(platform)}
-                type="button"
-              >
-                <Image
-                  src={platform.icon}
-                  width={16}
-                  height={16}
-                  alt="Platform"
-                />
-                {platform.label}
-              </button>
-            </li>
-          ))}
+        <ul
+          className="absolute z-20 mt-2 h-48 w-[calc(100%-3rem)] overflow-y-scroll rounded-lg border border-gray-border bg-white px-4 py-3 shadow tablet:w-[calc(100%-2.5rem)]"
+          ref={menuRef}
+        >
+          {platforms.map((platform, index) => {
+            const PlatformIcon = iconMap[platform.value];
+
+            return (
+              <li key={platform.value} className="text-gray-dark">
+                <button
+                  value={platform.value}
+                  className={`flex w-full items-center gap-3 border-b border-gray-border py-3 ${
+                    index === 0 && platforms.length > 1 ? "py-0 pb-3" : ""
+                  } ${
+                    index === platforms.length - 1 ? "border-b-0 py-0 pt-3" : ""
+                  } ${platforms.length === 1 ? "py-0 pb-0 pt-0" : ""}`}
+                  onClick={() => handleSelect(platform)}
+                  type="button"
+                >
+                  <PlatformIcon className="text-gray" />
+                  {platform.label}
+                </button>
+              </li>
+            );
+          })}
         </ul>
       )}
     </>
