6809a0ef964e9aa106f3173b50beff4e4e9f1188
Nicole Haraj <nicoleharaj@outlook.com>
2023-11-11 19:26:46 -0500

    extract component

---
+++ src/app/components/MainContent/index.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useState } from "react";
+import Mockup from "../Mockup";
+import SocialLinks, { SocialLink } from "../SocialLinks";
+import Container from "../UI/Container";
+
+export default function MainContent() {
+  const [links, setLinks] = useState<Array<SocialLink>>([]);
+
+  return (
+    <main className="flex h-[calc(100vh-102px)] items-start justify-center gap-6 p-4 tablet:p-6">
+      <Mockup links={links} />
+      <Container className="flex flex-col gap-6 p-0 tablet:p-10 desktop:min-h-full desktop:w-4/5">
+        <div className="flex flex-col gap-2 px-6 pt-6 tablet:px-0 tablet:pt-0">
+          <h1 className="text-heading-md-mobile font-bold tablet:text-heading-md-web">
+            Customize your links
+          </h1>
+          <p className="text-gray">
+            Add/edit/remove links below and then share all your profiles with
+            the world!
+          </p>
+        </div>
+
+        <SocialLinks setLinks={setLinks} links={links} />
+      </Container>
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,8 @@ import { Database } from "@/lib/types/supabase";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
-import Container from "./components/UI/Container";
 import Navbar from "./components/Navbar";
-import Mockup from "./components/Mockup";
-import SocialLinks from "./components/SocialLinks";
+import MainContent from "./components/MainContent";
 
 export default async function Home() {
   const supabase = createServerComponentClient<Database>({
@@ -23,22 +21,7 @@ export default async function Home() {
   return (
     <>
       <Navbar />
-      <main className="flex h-[calc(100vh-102px)] items-start justify-center gap-6 p-4 tablet:p-6">
-        <Mockup />
-        <Container className="flex flex-col gap-6 p-0 tablet:p-10 desktop:min-h-full desktop:w-4/5">
-          <div className="flex flex-col gap-2 px-6 pt-6 tablet:px-0 tablet:pt-0">
-            <h1 className="text-heading-md-mobile font-bold tablet:text-heading-md-web">
-              Customize your links
-            </h1>
-            <p className="text-gray">
-              Add/edit/remove links below and then share all your profiles with
-              the world!
-            </p>
-          </div>
-
-          <SocialLinks />
-        </Container>
-      </main>
+      <MainContent />
     </>
   );
 }
