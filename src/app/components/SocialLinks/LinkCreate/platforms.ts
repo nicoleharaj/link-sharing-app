8c2766054117e7eb3b26a0b5b5574faf54dd5c82
Nicole Haraj <nicoleharaj@outlook.com>
2023-11-11 19:23:32 -0500

    create icon map

--- src/app/components/SocialLinks/LinkCreate/platforms.ts
+++ src/app/components/SocialLinks/LinkCreate/platforms.ts
@@ -1,9 +1,39 @@
 import { validSocials } from "..";
+import {
+  GitHubIcon,
+  FrontendMentorIcon,
+  TwitterIcon,
+  LinkedInIcon,
+  YouTubeIcon,
+  FacebookIcon,
+  TwitchIcon,
+  DevToIcon,
+  CodewarsIcon,
+  FreeCodeCampIcon,
+  GitLabIcon,
+  HashnodeIcon,
+  StackOverflowIcon,
+} from "../../UI/SocialIcons";
+
+export const iconMap = {
+  github: GitHubIcon,
+  frontendMentor: FrontendMentorIcon,
+  twitter: TwitterIcon,
+  linkedIn: LinkedInIcon,
+  youTube: YouTubeIcon,
+  facebook: FacebookIcon,
+  twitch: TwitchIcon,
+  devTo: DevToIcon,
+  codewars: CodewarsIcon,
+  freeCodeCamp: FreeCodeCampIcon,
+  gitLab: GitLabIcon,
+  hashnode: HashnodeIcon,
+  stackOverflow: StackOverflowIcon,
+};
 
 export type platform = {
   value: validSocials;
   label: string;
-  icon: string;
   placeholder: string;
   regex: RegExp;
 };
@@ -12,92 +42,82 @@ export const platforms: Array<platform> = [
   {
     value: "github",
     label: "GitHub",
-    icon: "/images/icon-github.svg",
     placeholder: "github.com/johnappleseed",
-    regex: /^https:\/\/(www\.)?github\.com\/[a-zA-Z0-9](?:[a-zA-Z0-9]|-(?=[a-zA-Z0-9])){0,38}\/?$/
+    regex:
+      /^https:\/\/(www\.)?github\.com\/[a-zA-Z0-9](?:[a-zA-Z0-9]|-(?=[a-zA-Z0-9])){0,38}\/?$/,
   },
   {
     value: "frontendMentor",
     label: "Frontend Mentor",
-    icon: "/images/icon-frontend-mentor.svg",
     placeholder: "frontendmentor.io/profile/johnappleseed",
-    regex: /^https:\/\/(www\.)?frontendmentor\.io\/profile\/[a-zA-Z0-9_-]+\/?$/
+    regex: /^https:\/\/(www\.)?frontendmentor\.io\/profile\/[a-zA-Z0-9_-]+\/?$/,
   },
   {
     value: "twitter",
     label: "Twitter",
-    icon: "/images/icon-twitter.svg",
     placeholder: "twitter.com/johnappleseed",
-    regex: /^https:\/\/(www\.)?twitter\.com\/[a-zA-Z0-9_]{1,15}\/?$/
+    regex: /^https:\/\/(www\.)?twitter\.com\/[a-zA-Z0-9_]{1,15}\/?$/,
   },
   {
     value: "linkedIn",
     label: "LinkedIn",
-    icon: "/images/icon-linkedin.svg",
     placeholder: "linkedin.com/in/johnappleseed",
-    regex: /^https:\/\/(www\.)?linkedin\.com\/in\/[a-zA-Z0-9_-]+\/?$/
+    regex: /^https:\/\/(www\.)?linkedin\.com\/in\/[a-zA-Z0-9_-]+\/?$/,
   },
   {
     value: "youTube",
     label: "YouTube",
-    icon: "/images/icon-youtube.svg",
     placeholder: "youtube.com/@johnappleseed",
-    regex: /^https:\/\/(www\.)?youtube\.com\/(@[a-zA-Z0-9_-]+|[a-zA-Z0-9_-]+)\/?$/
+    regex:
+      /^https:\/\/(www\.)?youtube\.com\/(@[a-zA-Z0-9_-]+|[a-zA-Z0-9_-]+)\/?$/,
   },
   {
     value: "facebook",
     label: "Facebook",
-    icon: "/images/icon-facebook.svg",
     placeholder: "facebook.com/johnappleseed",
-    regex: /^https:\/\/(www\.)?facebook\.com\/[a-zA-Z0-9.]+\/?$/
+    regex: /^https:\/\/(www\.)?facebook\.com\/[a-zA-Z0-9.]+\/?$/,
   },
   {
     value: "twitch",
     label: "Twitch",
-    icon: "/images/icon-twitch.svg",
     placeholder: "twitch.tv/johnappleseed",
-    regex: /^https:\/\/(www\.)?twitch\.tv\/[a-zA-Z0-9_]+\/?$/
+    regex: /^https:\/\/(www\.)?twitch\.tv\/[a-zA-Z0-9_]+\/?$/,
   },
   {
     value: "devTo",
     label: "DEV Community",
-    icon: "/images/icon-devto.svg",
     placeholder: "dev.to/johnappleseed",
-    regex: /^https:\/\/(www\.)?dev\.to\/[a-zA-Z0-9_]+\/?$/
+    regex: /^https:\/\/(www\.)?dev\.to\/[a-zA-Z0-9_]+\/?$/,
   },
   {
     value: "codewars",
     label: "Codewars",
-    icon: "/images/icon-codewars.svg",
     placeholder: "codewars.com/users/johnappleseed",
-    regex: /^https:\/\/(www\.)?codewars\.com\/users\/[a-zA-Z0-9_-]+\/?$/
+    regex: /^https:\/\/(www\.)?codewars\.com\/users\/[a-zA-Z0-9_-]+\/?$/,
   },
   {
     value: "freeCodeCamp",
     label: "FreeCodeCamp",
-    icon: "/images/icon-freecodecamp.svg",
     placeholder: "freecodecamp.org/johnappleseed",
-    regex: /^https:\/\/(www\.)?freecodecamp\.org\/[a-zA-Z0-9]+\/?$/
+    regex: /^https:\/\/(www\.)?freecodecamp\.org\/[a-zA-Z0-9]+\/?$/,
   },
   {
     value: "gitLab",
     label: "GitLab",
-    icon: "/images/icon-gitlab.svg",
     placeholder: "gitlab.com/johnappleseed",
-    regex: /^https:\/\/(www\.)?gitlab\.com\/[a-zA-Z0-9_.-]+\/?$/
+    regex: /^https:\/\/(www\.)?gitlab\.com\/[a-zA-Z0-9_.-]+\/?$/,
   },
   {
     value: "hashnode",
     label: "Hashnode",
-    icon: "/images/icon-hashnode.svg",
     placeholder: "hashnode.com/profile/johnappleseed",
-    regex: /^https:\/\/(www\.)?hashnode\.com\/profile\/[a-zA-Z0-9_-]+\/?$/
+    regex: /^https:\/\/(www\.)?hashnode\.com\/profile\/[a-zA-Z0-9_-]+\/?$/,
   },
   {
     value: "stackOverflow",
     label: "Stack Overflow",
-    icon: "/images/icon-stack-overflow.svg",
     placeholder: "stackoverflow.com/users/12345/john-appleseed",
-    regex: /^https:\/\/(www\.)?stackoverflow\.com\/users\/\d+\/[a-zA-Z0-9_-]+\/?/
+    regex:
+      /^https:\/\/(www\.)?stackoverflow\.com\/users\/\d+\/[a-zA-Z0-9_-]+\/?/,
   },
 ];
